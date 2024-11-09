6083ba2fa8992b962fa3e752b7e64df58f7f12f0
Diegocabj97 <diegojadrian97@gmail.com>
2024-11-09 20:25:27 +0000

    Solucionando problema punto A

--- Punto_A.sh
+++ Punto_A.sh
@@ -45,9 +45,9 @@ FIN
 sudo pvcreate /dev/sdd1
 sudo vgcreate vg_temp /dev/sdd1
 
-sudo lvcreate -L +20m vg_datos -n lv_docker
-sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
-sudo lvcreate -L +512M vg_temp -n lv_swap
+sudo lvcreate -L +5M vg_datos -n lv_docker
+sudo lvcreate -L 1.5G vg_datos -n lv_workareas
+sudo lvcreate -l +100%FREE vg_temp -n lv_swap
 
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
