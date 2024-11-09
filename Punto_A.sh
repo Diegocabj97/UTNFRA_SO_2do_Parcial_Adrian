f97c1b99abefb7d40c3d59df51a9b69c1f6b5a12
Diegocabj97 <diegojadrian97@gmail.com>
2024-11-09 17:16:00 +0000

    Punto_A terminado

--- Punto_A.sh
+++ Punto_A.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 sudo fdisk /dev/sde << FIN
 n
 p
@@ -13,6 +11,7 @@ t
 8e
 wq
 FIN
+
 sudo pvcreate /dev/sde1
 sudo vgcreate vg_datos /dev/sde1
 
@@ -46,9 +45,9 @@ FIN
 sudo pvcreate /dev/sdd1
 sudo vgcreate vg_temp /dev/sdd1
 
-sudo lvcreate -L +1.5G vg_datos -n lv_workareas
-sudo lvcreate -L +512M vg_temp -n lv_swap
 sudo lvcreate -L +20m vg_datos -n lv_docker
+sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
+sudo lvcreate -L +512M vg_temp -n lv_swap
 
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
