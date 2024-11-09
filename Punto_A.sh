0be498a69be8c5a1277f3d6cfa22404e80250868
Diegocabj97 <diegojadrian97@gmail.com>
2024-11-09 17:13:55 +0000

    Punto_A terminado

---
+++ Punto_A.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#!/bin/bash
+
+sudo fdisk /dev/sde << FIN
+n
+p
+1
+
++5M
+y
+t
+8e
+wq
+FIN
+sudo pvcreate /dev/sde1
+sudo vgcreate vg_datos /dev/sde1
+
+sudo fdisk /dev/sde <<FIN
+n
+p
+
+
+
++1.5G
+t
+2
+8e
+wq
+FIN
+sudo pvcreate /dev/sde2
+sudo vgextend vg_datos /dev/sde2
+
+
+sudo fdisk /dev/sdd <<FIN
+n
+p
+
+
++512M
+y
+t
+8e
+wq
+FIN
+sudo pvcreate /dev/sdd1
+sudo vgcreate vg_temp /dev/sdd1
+
+sudo lvcreate -L +1.5G vg_datos -n lv_workareas
+sudo lvcreate -L +512M vg_temp -n lv_swap
+sudo lvcreate -L +20m vg_datos -n lv_docker
+
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkswap /dev/mapper/vg_temp-lv_swap
+
+cd
+sudo mkdir /work/
+sudo mount /dev/mapper/vg_datos-lv_workareas /work/
+sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
+free -h
+sudo swapon /dev/mapper/vg_temp-lv_swap
+
+
+sudo pvs
+sudo vgs
+sudo lvs
